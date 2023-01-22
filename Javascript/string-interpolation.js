17dfe7ac9af032d691dd6b8b58febac960b62126
Manjot Singh Gulati <manjotsinghgulati@hotmail.com>
2023-01-22 12:14:14 +0530

    JS String Interpolation

---
+++ Javascript/string-interpolation.js
@@ -0,0 +1,9 @@
+// String Interpolation
+
+let firstName = "Manjot";
+let lastName = "Singh";
+// let fullName = firstName + " " + lastName;
+let fullName = `${firstNmae} ${lastName}`;
+
+
+console.log("My name is " + fullName);
